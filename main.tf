43c58e7c601ddd313bb6871bda07783ad5a11e60
Chris Olido <chris.olido@gmail.com>
2023-08-07 15:35:27 +0800

    testing the private subnet

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_nat_gateway" "nat_gateway" {
     aws_eip.elastic_ip,
   ]
   allocation_id = aws_eip.elastic_ip.id
-  subnet_id     = aws_subnet.public_subnets.id
+  subnet_id     = element(aws_subnet.private_subnets[*].id, count.index)
 
   tags = {
     Name = "nat-gateway"
