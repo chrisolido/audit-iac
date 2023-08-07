47dabd5ec0068dd748e798d8d90a4ca3457d26a3
Chris Olido <chris.olido@gmail.com>
2023-08-07 14:45:40 +0800

    Testing variables

--- terraform_setup/main.tf
+++ terraform_setup/main.tf
@@ -6,6 +6,25 @@ resource "aws_vpc" "main" {
  }
 }
 
+# Variables
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+}
+
 resource "aws_subnet" "public_subnets" {
  count      = length(var.public_subnet_cidrs)
  vpc_id     = aws_vpc.main.id
