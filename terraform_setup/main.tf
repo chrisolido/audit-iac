94d5a1a1fbbc30190b0ed3f9f6ce6f82a71a5787
Chris Olido <chris.olido@gmail.com>
2023-08-07 13:15:14 +0800

    Terraform VPC, Subnets, IGW and NAT

---
+++ terraform_setup/main.tf
@@ -0,0 +1,102 @@
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
+
+# elastic ip
+resource "aws_eip" "elastic_ip" {
+  vpc      = true
+}
+
+# NAT gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on = [
+    aws_subnet.public_subnet,
+    aws_eip.elastic_ip,
+  ]
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# route table with target as NAT gateway
+resource "aws_route_table" "NAT_route_table" {
+  depends_on = [
+    aws_vpc.vpc,
+    aws_nat_gateway.nat_gateway,
+  ]
+
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "NAT-route-table"
+  }
+}
+
+# associate route table to private subnet
+resource "aws_route_table_association" "associate_routetable_to_private_subnet" {
+  depends_on = [
+    aws_subnet.private_subnet,
+    aws_route_table.NAT_route_table,
+  ]
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.NAT_route_table.id
+}
